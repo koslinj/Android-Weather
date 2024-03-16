720339166a2d4e79c86fb2f74bc09768420b34ac
koslinj <koslinj@gmail.com>
2024-03-16 13:32:33 +0100

    renamed for shorter name

--- app/src/main/java/koslin/jan/weather/config/Keys.kt
+++ app/src/main/java/koslin/jan/weather/config/Keys.kt
@@ -1,6 +1,6 @@
 package koslin.jan.weather.config
 
-object SharedPreferencesKeys {
+object Keys {
     const val LATITUDE_KEY = "latitude"
     const val LONGITUDE_KEY = "longitude"
     const val DEFAULT_CITY_KEY = "default_city"
