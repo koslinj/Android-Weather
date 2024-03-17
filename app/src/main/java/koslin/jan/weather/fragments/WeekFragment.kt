b79dc192f1b7effa6206c84e07a017efd7f1e411
koslinj <koslinj@gmail.com>
2024-03-17 19:26:10 +0100

    generally finished WeekFragment

--- app/src/main/java/koslin/jan/weather/fragments/WeekFragment.kt
+++ app/src/main/java/koslin/jan/weather/fragments/WeekFragment.kt
@@ -1,6 +1,7 @@
 package koslin.jan.weather.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.ProgressBar
@@ -15,11 +16,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.util.Date
-import java.util.Locale
-import kotlin.math.abs
 
 class WeekFragment : Fragment(R.layout.fragment_week) {
 
@@ -53,30 +49,24 @@ class WeekFragment : Fragment(R.layout.fragment_week) {
                 val temperatureUnit = uiState.temperatureUnit
 
                 CoroutineScope(Dispatchers.IO).launch {
-                    val filteredWeatherInfo = mutableListOf<SingleWeatherInfo>()
+                    val timeLen = weatherInfo[0].time.length
+                    val groupedWeatherInfo = mutableListOf<MutableList<SingleWeatherInfo>>()
 
-                    val currentDateTime = Calendar.getInstance()
-                    val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm", Locale.getDefault())
+                    var group = mutableListOf<SingleWeatherInfo>()
 
-                    repeat(7) { dayIndex ->
-                        val currentDay = currentDateTime.clone() as Calendar
-                        currentDay.add(Calendar.DAY_OF_YEAR, dayIndex)
-
-                        val dayWeatherInfo = weatherInfo.filter { weather ->
-                            val dateTime = dateFormat.parse(weather.time)
-                            val weatherDateTime = Calendar.getInstance()
-                            weatherDateTime.time = dateTime
-                            weatherDateTime.get(Calendar.DAY_OF_YEAR) == currentDay.get(Calendar.DAY_OF_YEAR) &&
-                                    (weatherDateTime.get(Calendar.HOUR_OF_DAY) == 10 ||
-                                            weatherDateTime.get(Calendar.HOUR_OF_DAY) == 15 ||
-                                            weatherDateTime.get(Calendar.HOUR_OF_DAY) == 20)
+                    for (item in weatherInfo){
+                        if(item.time.subSequence(timeLen-5, timeLen) == "10:00"){
+                            group.add(SingleWeatherInfo(item.time, item.temperature, item.rain))
+                        } else if(item.time.subSequence(timeLen-5, timeLen) == "15:00"){
+                            group.add(SingleWeatherInfo(item.time, item.temperature, item.rain))
+                        } else if(item.time.subSequence(timeLen-5, timeLen) == "20:00"){
+                            group.add(SingleWeatherInfo(item.time, item.temperature, item.rain))
+                            groupedWeatherInfo.add(group)
+                            group = mutableListOf()
                         }
-
-                        filteredWeatherInfo.addAll(dayWeatherInfo)
                     }
-
                     withContext(Dispatchers.Main) {
-                        updateUI(filteredWeatherInfo, temperatureUnit)
+                        updateUI(groupedWeatherInfo, temperatureUnit)
                         loadingProgressBar.visibility = View.GONE
                         recyclerView.visibility = View.VISIBLE
                     }
@@ -96,7 +86,7 @@ class WeekFragment : Fragment(R.layout.fragment_week) {
         }
     }
 
-    private fun updateUI(weatherInfo: List<SingleWeatherInfo>, temperatureUnit: String) {
+    private fun updateUI(weatherInfo: List<List<SingleWeatherInfo>>, temperatureUnit: String) {
         adapter.updateData(weatherInfo, temperatureUnit)
     }
 
--- app/src/main/java/koslin/jan/weather/fragments/WeeklyWeatherAdapter.kt
+++ app/src/main/java/koslin/jan/weather/fragments/WeeklyWeatherAdapter.kt
@@ -3,26 +3,40 @@ package koslin.jan.weather.fragments
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import koslin.jan.weather.R
 import koslin.jan.weather.data.SingleWeatherInfo
 
 class WeeklyWeatherAdapter(
-    private var weatherInfo: List<SingleWeatherInfo>,
+    private var weatherGroups:  List<List<SingleWeatherInfo>>,
     private var temperatureUnit: String
 ) :
     RecyclerView.Adapter<WeeklyWeatherAdapter.WeeklyForecastViewHolder>() {
 
     inner class WeeklyForecastViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val dateTextView: TextView = itemView.findViewById(R.id.dateTextView)
-        val tv1: TextView = itemView.findViewById(R.id.time1TextView)
-        val tv2: TextView = itemView.findViewById(R.id.time2TextView)
-        val tv3: TextView = itemView.findViewById(R.id.time3TextView)
+        val timeTv1: TextView = itemView.findViewById(R.id.time1TextView)
+        val timeTv2: TextView = itemView.findViewById(R.id.time2TextView)
+        val timeTv3: TextView = itemView.findViewById(R.id.time3TextView)
+        val tempTv1: TextView = itemView.findViewById(R.id.temp1TextView)
+        val tempTv2: TextView = itemView.findViewById(R.id.temp2TextView)
+        val tempTv3: TextView = itemView.findViewById(R.id.temp3TextView)
+        val rainTv1: TextView = itemView.findViewById(R.id.rain1TextView)
+        val rainTv2: TextView = itemView.findViewById(R.id.rain2TextView)
+        val rainTv3: TextView = itemView.findViewById(R.id.rain3TextView)
+        val img1: ImageView = itemView.findViewById(R.id.img1)
+        val img2: ImageView = itemView.findViewById(R.id.img2)
+        val img3: ImageView = itemView.findViewById(R.id.img3)
+
+        val views1: List<TextView> = listOf(timeTv1, tempTv1, rainTv1)
+        val views2: List<TextView> = listOf(timeTv2, tempTv2, rainTv2)
+        val views3: List<TextView> = listOf(timeTv3, tempTv3, rainTv3)
     }
 
-    fun updateData(newWeatherInfo: List<SingleWeatherInfo>, newTemperatureUnit: String) {
-        weatherInfo = newWeatherInfo
+    fun updateData(newWeatherGroups: List<List<SingleWeatherInfo>>, newTemperatureUnit: String) {
+        weatherGroups = newWeatherGroups
         temperatureUnit = newTemperatureUnit
         //WAŻNE
         notifyDataSetChanged()
@@ -36,31 +50,56 @@ class WeeklyWeatherAdapter(
 
     override fun onBindViewHolder(holder: WeeklyForecastViewHolder, position: Int) {
         // Bind the data to the views in each item
-        val len = weatherInfo[position].time.length
-        val onlyHour = weatherInfo[position].time.subSequence(len - 5, len)
-        holder.tv1.text = onlyHour
+        val weatherGroup = weatherGroups[position]
+        val len = weatherGroup[0].time.length
 
-        // Format temperature and rain values
-        val formattedTemperature = if (temperatureUnit == "celsius") {
-            String.format("%.1f°C", weatherInfo[position].temperature)
-        } else {
-            String.format("%.1f°F", weatherInfo[position].temperature)
+        holder.dateTextView.text = weatherGroup[0].time.subSequence(0, len - 6)
+        // For each slot (1, 2, 3)
+        for (i in 0..2) {
+            val onlyHour = weatherGroup[i].time.subSequence(len - 5, len)
+            val temperature = weatherGroup[i].temperature
+            val rain = weatherGroup[i].rain
+            val formattedTemperature = if (temperatureUnit == "celsius") {
+                String.format("%.1f°C", temperature)
+            } else {
+                String.format("%.1f°F", temperature)
+            }
+            val formattedRain = String.format("%.1fmm", rain)
+
+            // Set data to corresponding views based on slot number
+            when (i) {
+                0 -> {
+                    setProperViews(holder.views1, holder.img1, onlyHour, formattedTemperature, formattedRain, rain)
+                }
+                1 -> {
+                    setProperViews(holder.views2, holder.img2, onlyHour, formattedTemperature, formattedRain, rain)
+                }
+                2 -> {
+                    setProperViews(holder.views3, holder.img3, onlyHour, formattedTemperature, formattedRain, rain)
+                }
+            }
         }
-        val formattedRain = String.format("%.1fmm", weatherInfo[position].rain)
+    }
 
-        holder.tv2.text = formattedTemperature
-        holder.tv3.text = formattedRain
-
-        // Set weather icons based on rain conditions (for example)
-//        val rainAmount = weatherInfo[position].rain
-//        if (rainAmount > 0) {
-//            holder.weatherIconImageView.setImageResource(R.drawable.rain_icon)
-//        } else {
-//            holder.weatherIconImageView.setImageResource(R.drawable.sun_icon)
-//        }
+    private fun setProperViews(
+        views: List<TextView>,
+        img: ImageView,
+        onlyHour: CharSequence,
+        formattedTemperature: String,
+        formattedRain: String,
+        rain: Double
+    ) {
+        views[0].text = onlyHour
+        views[1].text = formattedTemperature
+        views[2].text = formattedRain
+        if (rain > 0) {
+            img.setImageResource(R.drawable.rain_icon)
+        } else {
+            img.setImageResource(R.drawable.sun_icon)
+        }
     }
 
     override fun getItemCount(): Int {
-        return weatherInfo.size
+        return weatherGroups.size
     }
 }
