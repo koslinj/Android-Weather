baebad57d0e7472c3067234c96a04ca5be11ae83
koslinj <koslinj@gmail.com>
2024-03-17 10:46:34 +0100

    update sharedPreferences keys

--- app/src/main/java/koslin/jan/weather/config/Keys.kt
+++ app/src/main/java/koslin/jan/weather/config/Keys.kt
@@ -6,5 +6,6 @@ object Keys {
     const val DEFAULT_CITY_KEY = "default_city"
     const val TEMPERATURE_UNIT_KEY = "temperature_unit"
     const val WIND_UNIT_KEY = "wind_unit"
+    const val FAVOURITE_CITIES_KEY = "favourite_cities"
     // Add more keys as needed
 }
