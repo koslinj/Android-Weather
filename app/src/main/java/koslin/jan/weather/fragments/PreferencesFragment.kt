7a35f98b708dc22608f9408eeca1fd3571cb3721
koslinj <koslinj@gmail.com>
2024-03-18 11:20:13 +0100

    preferences not changing when NO INTERNET CONNECTION

--- app/src/main/java/koslin/jan/weather/fragments/PreferencesFragment.kt
+++ app/src/main/java/koslin/jan/weather/fragments/PreferencesFragment.kt
@@ -3,7 +3,11 @@ package koslin.jan.weather.fragments
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import androidx.preference.EditTextPreference
+import androidx.preference.ListPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import koslin.jan.weather.R
 import koslin.jan.weather.WeatherViewModel
@@ -13,6 +17,7 @@ class PreferencesFragment : PreferenceFragmentCompat() {
 
     private lateinit var weatherViewModel: WeatherViewModel
     private lateinit var preferencesChangeListener: SharedPreferences.OnSharedPreferenceChangeListener
+    private var oldValueMap: MutableMap<String, Any?> = mutableMapOf()
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
@@ -20,10 +25,45 @@ class PreferencesFragment : PreferenceFragmentCompat() {
         weatherViewModel = ViewModelProvider(requireActivity(), WeatherViewModel.Factory)
             .get(WeatherViewModel::class.java)
 
+        for (key in weatherViewModel.defaultPreferences.all.keys) {
+            oldValueMap[key] = weatherViewModel.defaultPreferences.all[key]
+        }
+
+        var isFirstTime = true
+
         preferencesChangeListener = SharedPreferences.OnSharedPreferenceChangeListener { sharedPreferences, key ->
-            weatherViewModel.updateWindSpeedUnit()
-            weatherViewModel.updateTemperatureUnit()
-            weatherViewModel.getWeatherData()
+            if (weatherViewModel.isNetworkAvailable()) {
+                // If there's internet connection, update preferences and fetch weather data
+                weatherViewModel.updateWindSpeedUnit()
+                weatherViewModel.updateTemperatureUnit()
+                weatherViewModel.getWeatherData()
+            } else {
+                // If there's no internet connection, notify the user and revert preference change
+                if(isFirstTime){
+                    Toast.makeText(requireContext(), "No internet connection. Preferences cannot be changed.", Toast.LENGTH_SHORT).show()
+                    isFirstTime = false
+                } else {
+                    isFirstTime = true
+                }
+                // Revert the preference change if already made
+                val oldValue = oldValueMap[key]
+                when (oldValue) {
+                    is String -> {
+                        sharedPreferences.edit().putString(key, oldValue).apply()
+                        // Manually update the UI to reflect the reverted preference change using SimpleSummaryProvider
+                        findPreference<ListPreference>(key!!)?.apply {
+                            value = oldValue // Set the value back to the old value
+                        }
+                    }
+                    is Int -> {
+                        sharedPreferences.edit().putInt(key, oldValue).apply()
+                        // Manually update the UI to reflect the reverted preference change using SimpleSummaryProvider
+                        findPreference<ListPreference>(key!!)?.apply {
+                            value = oldValue.toString() // Set the value back to the old value
+                        }
+                    }
+                }
+            }
         }
     }
 
